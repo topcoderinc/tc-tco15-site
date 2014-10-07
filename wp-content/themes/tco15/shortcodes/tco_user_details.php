45c727acfc3f7341f73178f249558651166e37d8
James Marquez <jamesmarquez@gmail.com>
2014-10-08 04:29:58 +0800

    dev user details dynamic config update

--- wp-content/themes/tco15/shortcodes/tco_user_details.php
+++ wp-content/themes/tco15/shortcodes/tco_user_details.php
@@ -3,53 +3,67 @@
  * tco_button
  */
 function tco_user_details_function() {
+	global $site_options;
 	
 	$html		= '';
+	
+	$module 	= $site_options['module'];
+	$dsid 		= $site_options['dev_dsid'];
+	$c 			= $site_options['dev_c_user'];	
 	$cr 		= $_GET['cr'];
 	$cd 		= $_GET['cd'];	
 
-	$url 		= get_bloginfo ( 'stylesheet_directory' ) . "/includes/development_user.php?cd=" . $cd . "&cr=" . $cr;
+	$url 		= get_bloginfo ( 'stylesheet_directory' ) . "/includes/development_user.php?cd=$cd&cr=$cr&module=$module&dsid=$dsid&c=$c";
 	$response 	= wp_remote_get ( $url, array('timeout'=>60) );
 	
-	if ( is_wp_error($response) || !isset($response['body']) || (isset($response['body']) && $response['body']=='ERROR: 400') ) {
-		return '<hr /><div class="alert alert-info">User Data Not Found</div>';
+
+	// If error
+	if ( is_wp_error ( $response ) ) {
+		$return['html'] = '<hr /><div class="alert alert-danger">'. $response->get_error_message() .'</div>';
+		$return['vars'] = false;
+		return $return;
 	}
-		
+
+	// If no Result
+	if (! isset ( $response['body'] ) || $response['body']=='[]' ) {
+		$return['html'] = '<hr /><div class="alert alert-info">No Challenge Details for this User</div>';
+		$return['vars'] = false;
+		return $return;
+	}
+	
 	if ($response['response']['code'] == 200) {
-		if (substr ( $response ['body'], 0, 5 ) != "ERROR") {
-			$xml = simplexml_load_string ( $response ['body'] );
-			
-			$html = '
-				<table class="leaderboard-table table table-striped table-hover table-responsive">
-					<thead>
-						<tr>
-							<th>Project Name</th>
-							<th class="text-center">Project Version</th>
-							<th class="text-center">Submitted Date</th>
-							<th class="text-center">Score</th>
-							<th class="text-center">Placement</th>
-							<th class="text-center">Placement Points</th>
-						</tr>
-					</thead>
-					<tbody>
-			';
-			foreach ( $xml->children() as $row ) {
-				
-				$html .= '
+		$xml = simplexml_load_string ( $response ['body'] );
+
+		$html = '
+			<table class="leaderboard-table table table-striped table-hover table-responsive">
+				<thead>
 					<tr>
-						<td>'.$row->project_name.'</td>
-						<td class="text-center">'.$row->project_version.'</td>
-						<td class="text-center">'.$row->submit_date.'</td>
-						<td class="text-center">'.$row->score.'</td>
-						<td class="text-center">'.$row->placed.'</td>
-						<td class="text-center">'.number_format((float)$row->placement_points, 2).'</td>
-					</tr>';
-			}	
+						<th>Project Name</th>
+						<th class="text-center">Project Version</th>
+						<th class="text-center">Submitted Date</th>
+						<th class="text-center">Score</th>
+						<th class="text-center">Placement</th>
+						<th class="text-center">Placement Points</th>
+					</tr>
+				</thead>
+				<tbody>
+		';
+		foreach ( $xml->children() as $row ) {
 			
 			$html .= '
-					</tbody>
-				</table>';		
-		}
+				<tr>
+					<td>'.$row->project_name.'</td>
+					<td class="text-center">'.$row->project_version.'</td>
+					<td class="text-center">'.$row->submit_date.'</td>
+					<td class="text-center">'.$row->score.'</td>
+					<td class="text-center">'.$row->placed.'</td>
+					<td class="text-center">'.number_format((float)$row->placement_points, 2).'</td>
+				</tr>';
+		}	
+		
+		$html .= '
+				</tbody>
+			</table>';		
 	}
 	return $html;
 }
