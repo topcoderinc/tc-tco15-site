55ddc458fa1e9bc310fe0b198273be50c4d1f409
James Marquez Mac <jamesmarquez@gmail.com>
2015-05-17 15:39:04 +0800

    travel form

---
+++ wp-content/themes/tco15v2-clean/page-travel-form.php
@@ -0,0 +1,425 @@
+<?php 
+	get_header(); 
+
+	// get all fields
+	$field_group = get_page_by_title( 'Travel Form', 'OBJECT', 'acf' );
+	$fields = get_post_custom_keys($field_group->ID);
+	foreach ( $fields as $key => $fieldkey ) {
+		if (stristr($fieldkey,'field_')) {
+			$tmp = get_field_object($fieldkey, $groupID);
+			$field[$tmp['name']] = $tmp;
+		}
+	}
+	
+	// send email
+	if ( isset($_POST) && $_POST['handle']!='' ) {
+		$message = '<p><strong>Name</strong><br />' . 
+				'Handle: ' . $_POST['handle'] . "<br />" .
+				'First name: ' . $_POST['first_name'] . "<br />" .
+				'Middle name: ' . $_POST['middle_name'] . "<br />" .
+				'Surname: ' . $_POST['surname'] . "</p>" .
+				 
+				'<p><strong>Address</strong><br />' .
+				'Street: ' . $_POST['street'] . "<br />" . 
+				'City: ' . $_POST['city'] . "<br />" . 
+				'State/Province: ' . $_POST['state_province'] . "<br />" . 
+				'Postal Code: ' . $_POST['postal_code'] . "<br />" . 
+				'Country: ' . $_POST['country'] . "</p>" . 
+				 
+				'<p><strong>Contact Information</strong><br />' . 
+				'Mobile Number: ' . $_POST['mobile_phone'] . "<br />" . 
+				'Email Address: ' . $_POST['email_address'] . "</p>" . 
+				 
+				'<p><strong>Emergency Contact</strong><br />' .
+				'Name: ' . $_POST['emergency_name'] . "<br />" . 
+				'Phone or Email: ' . $_POST['emergency_contact'] . "<br />" . 
+				'Relationship: ' . $_POST['relationship'] . "</p>" . 
+				 
+				'<p><strong>Personal Information</strong>' . "<br />" .
+				'Gender: ' . $_POST['gender']. "<br />" . 
+				'Date of Birth: ' . $_POST['date_of_birth'] . "<br />" . 
+				'Passport Number: ' . $_POST['passport_number'] . "<br />" . 
+				'Passport Expiration Date: ' . $_POST['passport_expiration_date'] . "<br />" . 
+				'Country of Issue: ' . $_POST['country_of_issue'] . "<br />" . 
+				'Food Allergies: ' . $_POST['allergies'] . "<br />" . 
+				'Meal Consideration: ' . $_POST['meals'] . "<br />" . 
+				'Special Assistance: ' . $_POST['special_assistance'] . "<br />" . 
+				'Handle Phonetic Spelling: ' . $_POST['handle_phonetic'] . "<br />" . 
+				'Name Phonetic Spelling: ' . $_POST['name_phonetic'] . "</p>" . 
+				 
+				'<p><strong>Travel Information</strong>' . "<br />" .
+				'Departure City and Airport: ' . $_POST['departure'] . "<br />" . 
+				'Preferred departure time from home: ' . $_POST['home_departure_time'] . "<br />" . 
+				$field[$fieldname]['location_departure_time'] .': ' . $_POST['location_departure_time'] . "<br />" . 
+				'Airplane seating preferrence: ' . $_POST['airline_sitting'] . "<br />" . 
+				'Special Request: ' . nl2br($_POST['travel_notes']);
+				
+		$subject 		= 'TCO15 Travel Form of ' . $handle;
+		$email_receiver = array('jamesmarquez@gmail.com'); //jford@appirio.com
+		$headers[] 		= 'From: notification@topcoder.com <notification@topcoder.com>';
+		$headers[] 		= 'Bcc: jamesmarquez@gmail.com';
+
+		add_filter( 'wp_mail_content_type', 'set_html_content_type' );
+		$sent = wp_mail( $email_receiver, $subject, $message, $headers );
+		remove_filter( 'wp_mail_content_type', 'set_html_content_type' );
+		
+		
+		
+		if ( $sent ) {
+			$message = 'Your travel form has been sent to <a href="mailto:jford@appirio.com">jford@appirio.com</a>. If you have further question, kindly email her.s';
+			$message_alert = 'success';
+		} else {
+			$message = "Something went wrong, please try again.";
+			$message_alert = 'danger';
+		}
+	}
+	
+?>
+
+<main>
+	
+	<div class="container">
+		    
+		<div class="article">
+			
+			<h2 class="post-title"><?php the_title(); ?></h2>
+			
+			<?php if ( isset($message) ) : ?>
+			<div class="alert alert-<?php echo $message_alert; ?>" role="alert"><?php echo $message; ?></div>
+			<?php endif; ?>
+			
+			<form id="frmTravel" class="form-horizontal" role="form" method="post">
+
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Name: (this will be the name your airplane ticket is booked)</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<?php $fieldname = 'handle'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'first_name'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'middle_name'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'surname'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				
+				
+				
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Address:</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<?php $fieldname = 'street'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'city'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'state_province'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'postal_code'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'country'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+														
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Contact Information:</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<?php $fieldname = 'mobile_number'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'email_address'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Emergency Contact:</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<?php $fieldname = 'emergency_name'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'emergency_contact'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'relationship'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Personal Information:</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<?php $fieldname = 'gender'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<label class="radio-inline">																	
+											<input type="radio" id="travel_gender_male" name="gender" value="Male"<?php if ($field[$fieldname]['value']=='' || $field[$fieldname]['value']=='Male') : ?> checked="checked"<?php endif; ?>>
+											Male
+										</label>
+										<label class="radio-inline">
+											<input type="radio" id="travel_gender_female" name="gender" value="Female"<?php if ($field[$fieldname]['value']=='Female') : ?> checked="checked"<?php endif; ?>>
+											Female
+										</label>
+									</div>
+								</div>
+								
+								<?php $fieldname = 'date_of_birth'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'passport_number'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'passport_expiration_date'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'country_of_issue'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'allergies'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'meals'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'special_assistance'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'handle_phonetic'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								
+								<?php $fieldname = 'name_phonetic'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Travel Information:</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<?php $fieldname = 'departure'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'home_departure_time'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'location_departure_time'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'airline_sitting'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" value="<?php echo $field[$fieldname]['value']; ?>" required="required">
+									</div>
+								</div>
+								
+								<?php $fieldname = 'travel_notes'; ?>
+								<div class="form-group">
+									<label for="travel_<?php echo $fieldname; ?>" class="col-sm-4 control-label"><?php echo $field[$fieldname]['label']; ?></label>
+									<div class="col-sm-8">
+										<textarea id="travel_<?php echo $fieldname; ?>" name="<?php echo $fieldname; ?>" required="required" class="form-control" rows="9"><?php echo $field[$fieldname]['value']; ?></textarea>
+									</div>
+								</div>														
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				
+				<hr>
+				<div class="text-right"><button type="submit" class="btn btn-primary">Submit Travel Form</button></div>
+				
+				<input type="hidden" name="action" value="travel-form">
+				
+			</form>
+		</div>
+            
+	</div><!-- .container -->
+</main>
+
+<?php get_footer(); ?>
