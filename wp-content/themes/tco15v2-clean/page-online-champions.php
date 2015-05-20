6ccce9a6a4fafd68906762cc6bd77eb4d5de65bf
James Marquez Mac <jamesmarquez@gmail.com>
2015-05-20 22:10:38 +0800

    updated online champion page

--- wp-content/themes/tco15v2-clean/page-online-champions.php
+++ wp-content/themes/tco15v2-clean/page-online-champions.php
@@ -5,18 +5,23 @@
 	<?php get_template_part ( 'part-masthead' ); ?>
 	
 	<div class="container">
-            
+
 		<div class="article">
-		
 		<?php 
-			if (have_posts())  {
+			if (have_posts())  :
 				the_post(); 
-				echo '<h2 class="post-title">';
-				the_title();
-				echo '</h2>';
-			}
+				$parent_title = get_the_title($post->post_parent);
 		?>
-		
+			<h2 class="post-title">Congratulations to the <?php echo ucwords($parent_title); ?> Online Champions!</h2>
+			
+			<?php the_content(); ?>
+			
+			<hr />
+			
+			<h2 class="alt-section-title">Meet some of our Online Champions!</h2>
+			
+			
+		<?php endif; ?>
 			<div class="row">
 			<?php
 				
@@ -34,28 +39,18 @@
 				$query = new WP_Query( $args );
 				
 				// The Loop
-				if ( $query->have_posts() ) {
+				if ( $query->have_posts() ) :
 					$ctr = 0;
-					while ( $query->have_posts() ) {
+					while ( $query->have_posts() ) :
 						$query->the_post();
-						$ctr++;
 						$topcoder_meaning 	= get_field('topcoder_meaning');
 						$strPermalink 		= $topcoder_meaning!='' ? get_the_permalink() : 'javascript:;';
 						
-						if ( get_field('topcoder_meaning')!='' ) {
-							$handle = get_the_title();
-							
-							$arrInterview[$handle]['coverphoto']	= get_field('cover_photo');
-							$arrInterview[$handle]['questions'] 	= array( get_field_object('topcoder_meaning'),
-													 get_field_object('greatest_accomplishment'),
-													 get_field_object('hobbies'),
-													 get_field_object('best_tip'),
-													 get_field_object('topcoder_story')
-													);
-						}
+						if ($topcoder_meaning!='') : 
+							$ctr++;
 			?>
-				<div class="col-md-3 col-sm-4">
-					<div class="finalist-thumb">
+				<div class="col-sm-3">
+					<a href="<?php echo $strPermalink; ?>" class="finalist-thumb">
 					<?php if ( has_post_thumbnail() ) {
 						the_post_thumbnail('full', array('class' => 'img-responsive img-full'));
 					} else {
@@ -63,51 +58,23 @@
 					}
 					?>
 						<span><?php the_title(); ?></span>
-					</div>
+					</a>
 				</div>
 				
 				<?php if ( $ctr%4==0) : ?>
-				<div class="clearfix hide visible-md"></div>
-				<?php endif; ?>
-				
-				<?php if ( $ctr%3==0) : ?>
-				<div class="clearfix hide visible-sm"></div>
+				<div class="clearfix"></div>
 				<?php endif; ?>
 				
 			<?php
-					}
-				}
-				
+						endif;
+					endwhile;
+				endif;
+					
 				// Restore original Post Data
 				wp_reset_postdata();
 			?>
 			</div>
-		</div>
-		
-		<?php if ( isset($arrInterview) ) : $ctr = 0; ?>
-		<hr />
-		<h2 class="alt-section-title">INTERVIEWS</h2>
-		<?php foreach( $arrInterview as $handle=>$interview ) : $ctr++; ?>
-			
-			<h3 class="interview-title"><?php echo $handle; ?></h3>
-			
-			<?php if ($interview['coverphoto']) : ?>
-			<div class="col-sm-6 pull-right">
-			<img src="<?php echo $interview['coverphoto']['sizes']['large']; ?>" class="img-responsive img-thumbnail alignright" />
-			</div>
-			<?php endif; ?>
-			
-			<?php foreach( $interview['questions'] as $k=>$v ) : ?>
-			<strong><?php echo $v['label']; ?></strong>
-			<?php echo apply_filters('the_content', $v['value']); ?>
-			<?php endforeach; ?>
-			
-			<div class="clearfix"></div>
-			
-		<?php endforeach; ?>
-        <?php endif; ?>
-		
-		
+		</div><!-- .article -->
 		
 	</div><!-- .container -->
 </main>
