19bb2707ffece5d0910587a8d55980711c0a2212
James Marquez Mac <jamesmarquez@gmail.com>
2015-07-14 00:42:40 +0800

    script remove previously saved period 4 challenges of UI Design and IA tracks

---
+++ wp-content/themes/tco15v2-clean/page-remove-period4.php
@@ -0,0 +1,37 @@
+<?php
+	
+	global $current_user;
+	get_currentuserinfo(); 
+
+			
+	// get total number of challenge to process
+	$args = array (
+		'post_type'              => 'challenge',
+		'post_status'            => 'publish',
+		'posts_per_page'         => -1,
+		'meta_query'             => array(
+			array(
+				'key'       => 'track',
+				'value'     => 'ui-design',
+			),
+			array(
+				'key'       => 'period',
+				'value'     => '4',
+			),
+		),
+	);
+	$query = new WP_Query( $args );
+	
+		
+	if ( $query->have_posts() ) {
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			echo '[' .get_the_title() . '] - Deleted <br />';
+			wp_delete_post($post->ID, true);
+		}
+		
+	} else {
+		echo 'Nothing to show...';	
+	}
+		
+?>
