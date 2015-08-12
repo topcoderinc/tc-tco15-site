3d0dffa27c40d9ee3422c996ad865ed56c5ba0a5
James Marquez Mac <jamesmarquez@gmail.com>
2015-08-12 22:57:44 +0800

    Finalists template page

---
+++ wp-content/themes/tco15v2-clean/template-finalists-list.php
@@ -0,0 +1,82 @@
+<?php 
+/*
+Template Name: Finalists List
+*/
+get_header(); ?>
+
+<main>
+
+	<?php get_template_part ( 'part-masthead' ); ?>
+	
+	<div class="container">
+
+		<div class="article">
+		<?php 
+			if (have_posts())  :
+				the_post(); 
+				$parent_title = get_the_title($post->post_parent);
+		?>
+			<h3 class="post-title">Congratulations to the <?php echo ucwords($parent_title); ?> Finalists!</h3>
+			
+			<?php the_content(); ?>
+			
+			
+		<?php endif; ?>
+			<div class="row">
+			<?php
+				
+				// get all child pages
+				$args = array (
+					'post_type'			=> 'page',
+					'post_parent'       => $post->ID,
+					'post_status'       => 'publish',
+					'order'             => 'ASC',
+					'orderby'           => 'menu_order',
+					'posts_per_page'    => '-1',
+				);
+				
+				// The Query
+				$query = new WP_Query( $args );
+				
+				// The Loop
+				if ( $query->have_posts() ) :
+					$ctr = 0;
+					while ( $query->have_posts() ) :
+						$query->the_post();
+						$topcoder_meaning 	= get_field('topcoder_meaning');
+						$strPermalink 		= $topcoder_meaning!='' ? get_the_permalink() : 'javascript:;';
+						
+						if ($topcoder_meaning!='') : 
+							$ctr++;
+			?>
+				<div class="col-sm-3">
+					<a href="<?php echo $strPermalink; ?>" class="finalist-thumb">
+					<?php if ( has_post_thumbnail() ) {
+						the_post_thumbnail('full', array('class' => 'img-responsive img-full'));
+					} else {
+						echo '<img src="'.get_template_directory_uri().'/i/nophoto-finalists.png" class="img-responsive img-full" />';	
+					}
+					?>
+						<span><?php the_title(); ?></span>
+					</a>
+				</div>
+				
+				<?php if ( $ctr%4==0) : ?>
+				<div class="clearfix"></div>
+				<?php endif; ?>
+				
+			<?php
+						endif;
+					endwhile;
+				endif;
+					
+				// Restore original Post Data
+				wp_reset_postdata();
+			?>
+			</div>
+		</div><!-- .article -->
+		
+	</div><!-- .container -->
+</main>
+
+<?php get_footer(); ?>
